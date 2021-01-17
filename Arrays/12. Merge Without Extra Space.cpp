212ac8637d90eaf8e24d96a0b395986362dffdfc
ayush-kumar774 <ayushkumar1234@protonmail.com>
2021-01-17 17:03:19 +0530

    DSA Cracker Sheet

---
+++ Arrays/12. Merge Without Extra Space.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+        void merge(int arr1[], int arr2[], int n, int m)
+        {
+
+        }
+};
+
+int main()
+{
+        int testcases ;
+        cin >> testcases ;
+        while (testcases--)
+        {
+                int n , m ;
+                cin >> n >> m ;
+                int arr1[n] , arr2[m] ;
+                for (int i = 0 ; i < n ; i++) cin >> arr1[i] ;
+                for (int i = 0 ; i < m ; i++) cin >> arr2[i] ;
+                
+        }
+        
+}
--- Arrays/13. Merge Intervals.cpp
+++ Arrays/13. Merge Intervals.cpp
@@ -4,13 +4,57 @@ using namespace std;
 class Solution
 {
 public:
+        static bool compareVector ( const vector <int> &first , const vector <int> &second )
+        {
+                return first[0] < second[0] ;
+        }
         vector<vector<int>> merge(vector<vector<int>> &intervals)
         {
+                sort(intervals.begin() , intervals.end(), compareVector) ;
 
+                int index = 0 ;
+                vector < vector<int> > res ;
+
+                while (index < intervals.size())
+                {
+                        if (index == intervals.size() - 1 or intervals[index][1] < intervals[index + 1][0])
+                        {
+                                res.push_back(intervals[index]) ;
+                                index++ ;
+                        }
+                        else
+                        {
+                                int bound = index + 1 ;
+                                intervals[bound][0] = intervals[index][0] ;
+                                intervals[bound][1] = max(intervals[bound][1] , intervals[index][1]) ;
+                                index = bound ;
+                        }
+                        
+                }
+                return res ;
         }
 };
 
 int main()
 {
-        
+        int row, column ;
+        cin >> row >> column ;
+        vector < vector <int> > intervals(row, vector<int> (column, 0)) , answer ;
+        for (int i = 0 ; i < row ; i++)
+        {
+                for (int j = 0 ; j < column ; j++)
+                {
+                        cin >> intervals[i][j] ;
+                }
+        }
+        Solution sol ;
+        answer = sol.merge(intervals) ;
+        for (int i = 0 ; i < answer.size() ; i++)
+        {
+                for (int j = 0 ; j < answer[i].size() ; j++)
+                {
+                        cout << answer[i][j] << " " ;
+                }
+                cout << endl ;
+        }
 }
